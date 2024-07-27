58e03c3cadfbd8723e8d3213e9e423cba62862bc
CodeofGuff <guffcode@gmail.com>
2024-07-27 13:15:28 -0700

    Day 12 complete!
    Classes. Lots to know about these and structs and enums

--- Reviews.swift
+++ Reviews.swift
@@ -16,10 +16,40 @@
 
 // Checkpoint 5
 // using sorted() filter() map()
-
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 luckyNumbers 
     .filter { $0 % 2 != 0} // filter even nums
     .sorted() // sorts
     .map { "\($0) is a lucky number!"} // maps them to srring
     .forEach { print($0) } // prints
+
+
+// Checkpoint 7
+// I left this as a  basic skeleton. I wan to add more to it later on
+class Animal {
+    var legs = 4
+}
+class Dog: Animal {
+    let isGoodDog = true
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+
+    func speak() {
+        print("Woof!")
+    }
+}
+class Cat: Animal {
+    var isTame: Bool
+    init(isTame: Bool) {
+        self.isTame = isTame
+    }
+    
+    func speak() {
+        print("Meo")
+    }
+}
+class Corgi: Dog { override func speak() { print("Yap yap yap!") } }
+class Tuxedo: Cat { init() { super.init(isTame: true) }; override func speak() { print("Puurraaahhhhh") } }
+
