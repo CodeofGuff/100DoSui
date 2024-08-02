5fcfb18e9feb6e9cae30bdeb7e80a6560269e74d
CodeofGuff <guffcode@gmail.com>
2024-08-02 11:03:37 -0700

    okay

---
+++ Day15_16.swift
@@ -0,0 +1,5 @@
+// Day15 was mainly review
+
+// Day16
+// Explanation of what the next days are going to look like
+// learn the basics of UI design, how to let users enter values and select from options, and how to track program state.
