f5d1bbddde88d5a471eb58e1dc88f38616521dbb
CodeofGuff <guffcode@gmail.com>
2024-08-02 11:37:12 -0700

    OK

--- Day15_16.swift
+++ Day15_16.swift
@@ -3,3 +3,4 @@
 // Day16
 // Explanation of what the next days are going to look like
 // learn the basics of UI design, how to let users enter values and select from options, and how to track program state.
+
--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -7,32 +7,25 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Form {
-            Section {
-                Text("Hello world!")
-                Text("Hello world!")
-                Text("Hello world!")
-            }
-            Section {
-                Text("Hello world!")
-                Text("Hello world!")
-                Text("Hello world!")
-                Text("Hello world!")
-                Text("Hello world!")
-            }
-            Text("Hello world!")
-            Text("Hello world!")
-            Text("Hello world!")
-            Text("Hello world!")
-            Text("Hello world!")
-            Text("Hello world!")
-            Text("Hello world!")
-            
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
